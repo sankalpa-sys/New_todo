520214791df555f49d11780ba8db37d1a3a70aa3
Sankalpa Neupane <sankalpaneupane@Sankalpas-MacBook-Pro.local>
2020-10-07 15:55:01 +0545

    username and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   has_many :todos
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
